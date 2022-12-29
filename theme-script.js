e31088a622861c4f053d9bda53a249e1a7a3ae95
MuBBaSher-YaSiN <mubbasheryasin125@gmail.com>
2022-12-29 11:22:14 +0500

    MuBBaSher YaSiN Portfolio Website Published

---
+++ theme-script.js
@@ -0,0 +1,14 @@
+var themeIcon = document.getElementById("theme-icon")
+var mode = document.getElementById("light-dark-mode")
+themeIcon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme"))
+    {
+        themeIcon.src = "./assets/images/sun.png"
+    }
+    else
+    {
+        themeIcon.src = "./assets/images/moon.png"
+    }
+}
